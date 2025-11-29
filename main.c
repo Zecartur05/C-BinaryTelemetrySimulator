e8a37405b0016023bd7a1cf303fdb404da8bbab9
Zecartur05 <108401276+Zecartur05@users.noreply.github.com>
2025-11-29 18:43:31 +0000

    ler do ficheiro implementado

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 
 
 #define TempBits 7
+#define TempMask 0x7F
+#define BatMask 0x0F
+#define ErroMask 0x01
 #define BateriaBits 4
 #define Erro 1
 
@@ -11,23 +14,39 @@ struct  informacao
     uint16_t info;
 }INFORMACAO;
 
-void lerFicheiro(){
-    
+void lerFicheiro(struct informacao *data){
+    FILE *f;
+    f = fopen("telemetria.bin","rb");
+    if(f == NULL){
+        perror("Erro abrir o ficheiro");
+    }
+    fread(data, sizeof(struct informacao),1,f);
+    fclose(f);
 }
 
-void guardarficheiro(struct informacao data){
+
+
+void printardados(struct informacao *data){
+    printf("valor Erro:%d\n",data->info & ErroMask);
+    printf("valor Erro:%x\n",data->info & ErroMask);
+    printf("valor Bateria:%d\n",data->info>>Erro & BatMask);
+    printf("valor Bateria:%x\n",data->info>>Erro & BatMask);
+    printf("valor Temperatura:%d\n",(data->info>> (Erro + BateriaBits)) & TempMask);
+    printf("valor Temperatura:%u\n",(data->info>> (Erro + BateriaBits)) & TempMask);
+}
+
+void guardarficheiro(struct informacao *data){
     FILE *f;
     f = fopen("telemetria.bin","wb");
     if (f == NULL) {
         printf("Erro ao abrir o ficheiro\n");
         return;
     }
-    fwrite(&data, sizeof(struct informacao), 1, f);
+    fwrite(data, sizeof(struct informacao), 1, f);
     fclose(f);
 }
 
-void guardardados(){
-    struct informacao data;
+void guardardados(struct informacao *data){
     int temp,bat,err;
     do{
         printf("Temperatura(entre 0 e 100)");
@@ -42,19 +61,12 @@ void guardardados(){
         scanf("%d",&err);
     }while(err < 0 || err > 1);
     
-    data.info = temp;
-    data.info = data.info << BateriaBits;
-    data.info = data.info | bat;
-    data.info = data.info << Erro;
-    data.info = data.info | err;
-    printf("Valor Final: %d\n",data.info);
-    guardarficheiro(data);
-    printf("--------------------\n");
-    printf("valor Erro:%d\n",data.info & Erro);
-    printf("valor Bateria:%d\n",data.info>>Erro & 0x0F);
-    printf("valor Temperatura:%d\n",(data.info>> (Erro + BateriaBits)) & 0x7F);
-
-
+    data->info = temp;
+    data->info = data->info << BateriaBits;
+    data->info = data->info | bat;
+    data->info = data->info << Erro;
+    data->info = data->info | err;
+    printf("Valor Final: %d\n",data->info);
 }
 
 
@@ -68,15 +80,18 @@ void menu(){
 
 int main(){
     int op = 0;
+    struct informacao data;
     do{
         menu();
         scanf("%d",&op);
         switch (op){
         case 1:
-            guardardados();
+            guardardados(&data);
+            guardarficheiro(&data);
             break;
         case 2:
-            lerFicheiro();
+            lerFicheiro(&data);
+            printardados(&data);
             break;
         case 9:
             break;
